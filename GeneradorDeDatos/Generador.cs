4ae7b68e506436519d8b8efb8ce5072e6f9908e5
jesus santiago <warner.santiago.03@gmail.com>
2018-06-03 13:30:50 -0500

    modificacion

---
+++ GeneradorDeDatos/Generador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneradorDeDatos
+{
+    class Generador
+    {
+        /*IManejadorTorneo manejadorTorneo;
+   public Generador()
+   {
+       Puntos = new List<Puntos>();
+       List NombreDeportes = new List<NombreDeportes>();
+       manejadorTorneo = new ManejadorTorneo(new RepositorioTorneo());
+   }
+
+   public List<Puntos> Puntos { get; set; }
+
+   private double Evaluar(double x)//aqui pasare mi lista de puntos
+   {
+       return Math.Pow(x, 2);
+   }
+
+   public List<Puntos> GeneradorDatos(double limiteInferior, double limiteSuperior, double incremento)
+   {
+       Puntos = new List<Puntos>();
+       for (double x = limiteInferior; x < limiteSuperior; x += incremento)
+       {
+           Puntos.Add(new Puntos(x, Evaluar(x)));
+       }
+       return Puntos;
+   }
+
+   public void GenerarListas(string deporte, string fecha)
+   {
+       foreach (var item in manejadorTorneo.Lista)
+       {
+           if (item.Tipo_Deporte == Deporte && item.FechaProgramada == Fecha)
+           {
+               NombreDeportes a = new NombreDeportes();
+               a.Nombre = item.Equipo1;
+               a.Numerador = NombreDeportes.Count + 1;
+               NombreDeportes.Add(a);
+           }
+           if (item.Tipo_Deporte == Deporte && item.FechaProgramada == Fecha)
+           {
+               NombreDeportes a = new NombreDeportes();
+               a.Nombre = item.Equipo2;
+               a.Numerador = nombreDeporte.Count + 1;
+               nombreDeporte.Add(a);
+           }
+       }
+   }
+  */
+    }
+}
---
+++ GeneradorDeDatos/Puntos2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneradorDeDatos
+{
+   public  class Puntos2
+    {
+        public double X { get; set; }
+        //  public string NombreDeporte { get; set; }
+        public double Y { get; set; }
+        public Puntos2(double x, double y)
+        {
+            X = x;
+            Y = y;
+            //    NombreDeporte = deporte;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+    }
+}
